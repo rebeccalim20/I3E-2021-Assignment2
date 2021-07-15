f6f2d696309952bb9d04408d7cff56d5d00d96ca
ohhLindsay <s10208348@connect.np.edu.sg>
2021-07-15 15:20:46 +0800

    adding musicplayer scene, adding music to scene

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+  void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
