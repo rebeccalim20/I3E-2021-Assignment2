3868534aeebed64665b617218955edd5a4a88ffc
rebeccalim20 <s10207898@connect.np.edu.sg>
2021-07-18 03:07:59 +0800

    update audio menu done

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -18,15 +18,30 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    
+    AudioSource audiosource;
+    public AudioClip hoversound;
+    public AudioClip clicksound;
+
     private void Start()
     {
+        audiosource = GetComponent<AudioSource>();
         if (SamplePlayer.lockCursor)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
     }
+
+    public void hoversounds()
+    {
+        audiosource.PlayOneShot(hoversound,0.7F);
+    }
+    public void clicksounds()
+    {
+        audiosource.PlayOneShot(clicksound, 0.7F);
+    }
+
+
     //load menu 
     public void loadmenu(string menu)
     {
